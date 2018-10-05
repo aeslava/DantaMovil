8996e84264ef1b6e95394cda4815e23f606fd7ec
alex eslava <eslava.alex@gmail.com>
2018-10-04 20:49:17 -0500

    subir proyecto movil

---
+++ src/pages/home/home.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import { Platform, ActionSheetController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public platform: Platform,
+    public actionsheetCtrl: ActionSheetController) {
+
+  }
+
+  openMenu() {
+    let actionSheet = this.actionsheetCtrl.create({
+      title: 'Navega por nuestro proyecto',
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Personas',
+          role: 'destructive',
+          icon: !this.platform.is('ios') ? 'trash' : null,
+          handler: () => {
+            console.log('Delete clicked');
+          }
+        },
+        {
+          text: 'Entidad',
+          icon: !this.platform.is('ios') ? 'share' : null,
+          handler: () => {
+            console.log('Share clicked');
+          }
+        },
+        {
+          text: 'Curso',
+          icon: !this.platform.is('ios') ? 'arrow-dropright-circle' : null,
+          handler: () => {
+            console.log('Play clicked');
+          }
+        },
+        {
+          text: 'Inscripcion',
+          icon: !this.platform.is('ios') ? 'heart-outline' : null,
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        },
+        {
+          text: 'Localizacion',
+          role: 'cancel', // will always sort to be on the bottom
+          icon: !this.platform.is('ios') ? 'close' : null,
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+}
+
+
+}
